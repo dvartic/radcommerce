2597ecc05a65be23f957e049e71b22e4c78794a2
Dvartic <agcascallar@gmail.com>
2024-10-27 11:04:22 +0100

    remove image conversion from migration

--- priv/repo/migrations/20241025083717_convert_images_to_avif.exs
+++ priv/repo/migrations/20241025083717_convert_images_to_avif.exs
@@ -11,41 +11,5 @@ defmodule PhoenixDemo.Repo.Migrations.ConvertImagesToAvif do
       FROM unnest(images) AS image
     )
     """
-
-    convert_images_to_avif()
-  end
-
-  defp convert_images_to_avif do
-    # Define the directory path
-    uploads_dir = Path.join([:code.priv_dir(:phoenix_demo), "static", upload_dir()])
-
-    # Read all files from the directory
-    files = File.ls!(uploads_dir)
-
-    # Process each file
-    Enum.each(files, fn file ->
-      case String.ends_with?(file, ".avif") do
-        true ->
-          nil
-
-        false ->
-          file_path = Path.join(uploads_dir, file)
-          dest = Path.join(uploads_dir, Path.rootname(file) <> ".avif")
-
-          # Assume that the file is an image
-
-          {:ok, image} = Image.open(file_path)
-
-          image
-          |> Image.write!(dest,
-            quality: 60,
-            webp: [minimize_file_size: true, effort: 10],
-            heif: [minimize_file_size: true, effort: 8]
-          )
-
-          # Remove original file
-          File.rm!(file_path)
-      end
-    end)
   end
 end
