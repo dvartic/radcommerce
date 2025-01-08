7d4410d07282e35e9e52ad9fe3ac8b7e3e90a976
Dvartic <agcascallar@gmail.com>
2025-01-08 13:25:27 +0100

    fix translations bug when only partial translations are added

--- lib/phoenix_demo/resolve_translations.ex
+++ lib/phoenix_demo/resolve_translations.ex
@@ -4,10 +4,17 @@ defmodule PhoenixDemo.ResolveTranslations do
   def resolve_text_content(%TextContent{} = text_content, locale) do
     if Map.has_key?(text_content, :translations) && is_list(text_content.translations) &&
          text_content.translations != [] do
-      Enum.find(text_content.translations, text_content.original_text, fn translation ->
-        translation.language.code == locale
-      end)
-      |> Map.get(:translated_text, text_content.original_text)
+      translation =
+        Enum.find(text_content.translations, text_content.original_text, fn translation ->
+          translation.language.code == locale
+        end)
+
+      # Determine if the translation map is found or not
+      case translation do
+        %{} = translation -> Map.get(translation, :translated_text, text_content.original_text)
+        # Other will be the default set in the previous Enum.find
+        other -> other
+      end
     else
       text_content.original_text
     end
