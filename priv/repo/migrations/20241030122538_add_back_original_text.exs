f8592b159545444034ca322840ddd26bcc540950
Dvartic <agcascallar@gmail.com>
2025-01-08 12:45:55 +0100

    modify migration to support text

--- priv/repo/migrations/20241030122538_add_back_original_text.exs
+++ priv/repo/migrations/20241030122538_add_back_original_text.exs
@@ -3,7 +3,7 @@ defmodule PhoenixDemo.Repo.Migrations.AddBackOriginalText do
 
   def change do
     alter table(:text_contents) do
-      add :original_text, :string
+      add :original_text, :text
     end
   end
 end
